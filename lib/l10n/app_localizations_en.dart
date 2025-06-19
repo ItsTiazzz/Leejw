44c7d04b7667c20a3ebdc8276674857be8bbfe15
Tiazzz <163735662+ItsTiazzz@users.noreply.github.com>
2025-06-19 22:55:53 +0200

    i18n

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get dashboard_title => 'Dashboard';
+
+  @override
+  String get dashboard_goto => 'Go to Dashboard';
+
+  @override
+  String get flashcards_title => 'Flashcards';
+
+  @override
+  String get flashcards_goto => 'Go to Flashcards';
+
+  @override
+  String get lessons_title => 'Lessons';
+
+  @override
+  String get lessons_goto => 'Go to your Lessons';
+
+  @override
+  String get settings_title => 'Settings';
+
+  @override
+  String get settings_goto => 'Go to Settings';
+
+  @override
+  String get voced_title => 'VocEd';
+
+  @override
+  String get voced_goto => 'Go to the VocEd';
+}
---
+++ lib/l10n/app_localizations_nl.dart
@@ -0,0 +1,40 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Dutch Flemish (`nl`).
+class AppLocalizationsNl extends AppLocalizations {
+  AppLocalizationsNl([String locale = 'nl']) : super(locale);
+
+  @override
+  String get dashboard_title => 'Overzicht';
+
+  @override
+  String get dashboard_goto => 'Ga naar het Overzicht';
+
+  @override
+  String get flashcards_title => 'Flitskaartjes';
+
+  @override
+  String get flashcards_goto => 'Ga naar de Flitskaartjes';
+
+  @override
+  String get lessons_title => 'Lessen';
+
+  @override
+  String get lessons_goto => 'Ga naar jouw Lessen';
+
+  @override
+  String get settings_title => 'Instellingen';
+
+  @override
+  String get settings_goto => 'Ga naar de Instellingen';
+
+  @override
+  String get voced_title => 'VocEd';
+
+  @override
+  String get voced_goto => 'Ga naar de VocEd';
+}
